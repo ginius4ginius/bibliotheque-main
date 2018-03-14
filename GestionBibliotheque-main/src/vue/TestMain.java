b4a4362644c1fe75ccf569bc8d1b6d264e350de0
ginius4ginius <derieu.vincent@outlook.com>
2018-03-14 17:25:04 +0100

    commit du soir du 14/03/18 par vincent

--- GestionBibliotheque-main/src/vue/TestMain.java
+++ GestionBibliotheque-main/src/vue/TestMain.java
@@ -3,6 +3,7 @@ package vue;
 import java.time.LocalDate;
 import java.util.Date;
 
+import model.Adherent;
 import model.AppVincent;
 import model.Genre;
 import model.Livre;
@@ -16,14 +17,47 @@ public class TestMain {
 	LocalDate datePret = LocalDate.now();
 	Date date = new Date(24 * 3600 * 1000 * datePret.toEpochDay());
 	
-	LocalDate dateRetour = LocalDate.now().plusWeeks(3);
-	Date date2 = new Date(24 * 3600 * 1000 * dateRetour.toEpochDay());
-	
 	//instanciation des éléments:
 	
 	Livre l1 = new Livre("2-07-038692-6", "Java pour les nuls", "Français", 80f, (short)2018);
 	Genre g1 = new Genre("Ecchi");
-	Pret p1 = new Pret(date, date2, null);
+	Adherent a1 = new Adherent("Derieu","Vincent", "0631618521","ginius4@hotmail.com",93470, "3 rue des grands champs", "Coubron");
+	
+	Pret p1 = new Pret(l1,a1,date);
+	
+	System.out.println("affichage des genres :");
+	managerVincent.addGenre(g1);
+	managerVincent.affichageDesGenres();
+	
+	System.out.println("affichage des livres :");
+	managerVincent.addLivre(l1);
+	managerVincent.affichageLivres();
+	
+	System.out.println("affichage des livres par genre :");
+	managerVincent.affichageLivresParGenre(g1);
+	
+	System.out.println("affichage des prets par adherent :"+a1.getNom());
+	managerVincent.addPret(p1, l1, a1);
+	managerVincent.affichagePretAdherent(a1);
+	
+	System.out.println("affichage des prets en cour :");
+	managerVincent.affichagePretEnCour();
+	
+	System.out.println("affichage des prets non rendu :");
+	managerVincent.AffichagePretNonRendu();
+	
+	System.out.println("affichage des prets en cour après remise du pret:");
+	managerVincent.removePret(p1);
+	managerVincent.affichagePretEnCour();
+	
+	System.out.println("affichage des livres après remise après supression du livre :");
+	managerVincent.removeLivre(l1);
+	managerVincent.affichageLivres();
+	
+	System.out.println("affichage des genres après supression du genre :");
+	managerVincent.removeGenre(g1);
+	managerVincent.affichageDesGenres();
+	
 
 	}
 
