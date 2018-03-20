2228e024cbaea230ca183ee55123caaa7a76e752
xuanbichmai <xuanbichmai@gmail.com>
2018-03-20 15:22:22 +0100

    TestMainMai

---
+++ GestionBibliotheque-main/src/vue/TestMainMai.java
@@ -0,0 +1,18 @@
+package vue;
+
+import model.AppMai;
+
+public class TestMainMai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AppMai controleurMai = new AppMai();
+		
+		
+		
+		controleurMai.affichageAdherents();
+		
+	}
+
+}
